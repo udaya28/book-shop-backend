a133ae187e6a822be948ac5db14022c8d5e887f7
udaya28 <54870625+udaya28@users.noreply.github.com>
2021-07-15 13:12:11 +0530

    refactored code

--- src/routes/mongo-updated/controllers/author.controller.ts
+++ src/routes/mongo-updated/controllers/author.controller.ts
@@ -2,14 +2,9 @@ import { Response, Request } from 'express';
 import mongoose from 'mongoose';
 import { AuthorDBNew } from './../../../model/mongo-updated/authors.model.mongo';
 import { AuthorController, AuthorNew, BookResult } from './../interface'
-import { createResponse, isValidAuthorId, isValidMongoIdUpdated } from '../util';
+import { createResponse, fetchAuthorId, isValidAuthorName } from '../util';
 import { ApiResponse, DeleteResult } from '../interface';
 import { handelError } from '../handel-error';
-
-const validateAuthorId = async (authorId: string) => {
-    isValidMongoIdUpdated(authorId)
-    await isValidAuthorId(authorId)
-}
 class AuthorUpdatedController implements AuthorController {
     constructor() { }
 
@@ -31,8 +26,7 @@ class AuthorUpdatedController implements AuthorController {
         let status;
         let response: ApiResponse<any>
         try {
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const author: Array<AuthorNew> = await AuthorDBNew.find({ _id: authorId });
             status = 200
             response = createResponse(author, 'Author Fetched')
@@ -48,6 +42,7 @@ class AuthorUpdatedController implements AuthorController {
         let response: ApiResponse<any>
         try {
             const authorName: string = req.body.data.authorName
+            await isValidAuthorName(authorName)
             const authorCountry: string = req.body.data.authorCountry
             const author: AuthorNew = await AuthorDBNew.create({
                 authorName,
@@ -63,18 +58,19 @@ class AuthorUpdatedController implements AuthorController {
     }
 
     updatedAuthorById = async (req: Request, res: Response): Promise<Response> => {
-
         let response: ApiResponse<any>;
         let status: number;
         try {
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const authorName: string = req.body.data.authorName
+            await isValidAuthorName(authorName)
             const authorCountry: string = req.body.data.authorCountry
-            const author: mongoose.UpdateWriteOpResult = await AuthorDBNew.updateOne({ _id: authorId }, {
-                authorName,
-                authorCountry,
-            });
+            let data: any = {}
+            if (authorName) data['authorName'] = authorName
+            if (authorCountry) data['authorCountry'] = authorCountry
+            const author: any = await AuthorDBNew.findByIdAndUpdate({ _id: authorId }, {
+                $set: data
+            }, { new: true });
             status = 200
             response = createResponse(author, 'Author Updated')
         } catch (error) {
@@ -88,8 +84,7 @@ class AuthorUpdatedController implements AuthorController {
         let response: ApiResponse<any>;
         let status: number;
         try {
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const result: DeleteResult = await AuthorDBNew.deleteOne({ _id: authorId });
             status = 200
             response = createResponse(result, 'Author Deleted')
@@ -105,8 +100,7 @@ class AuthorUpdatedController implements AuthorController {
         let response: ApiResponse<any>;
         let status: number;
         try {
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const books: Array<BookResult> = await AuthorDBNew.find({ _id: authorId }, '-_id books');
             status = 200
             response = createResponse(books, 'All books By author Fetched')
@@ -121,8 +115,7 @@ class AuthorUpdatedController implements AuthorController {
         let response: ApiResponse<any>;
         let status: number;
         try {
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const bookTitle: string = req.params.bookTitle || ""
             const book: Array<BookResult> = await AuthorDBNew.find({ _id: authorId }, `-_id books.${bookTitle}`);
             status = 200
@@ -138,14 +131,37 @@ class AuthorUpdatedController implements AuthorController {
         let response: ApiResponse<any>;
         let status: number;
         try {
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const title: string = req.body.data.title
             const publishedOn: string = req.body.data.publishedOn
             const genres: Array<string> = req.body.data.genres
-            await AuthorDBNew.findOneAndUpdate({ _id: authorId }, { [`books.${title}`]: { title, publishedOn, genres } });
+            const result = await AuthorDBNew.findOneAndUpdate({ _id: authorId }, { [`books.${title}`]: { title, publishedOn, genres } }, { new: true });
             status = 200
-            response = createResponse(null, 'Added Book to Author')
+            response = createResponse(result, 'Added Book to Author')
+        } catch (error) {
+            status = 500
+            response = handelError(error)
+        }
+        return res.send(response).status(status);
+    }
+
+    updateBookByAuthorIdAndAuthorId = async (req: Request, res: Response): Promise<Response> => {
+        let response: ApiResponse<any>;
+        let status: number;
+        try {
+            const authorId = await fetchAuthorId(req)
+            const bookTitle: string = req.params.bookTitle
+            const title: string = req.body.data.title
+            const publishedOn: string = req.body.data.publishedOn
+            const genres: Array<string> = req.body.data.genres
+            let data: any = {}
+            if (title) data['title'] = title;
+            if (publishedOn) data['publishedOn'] = publishedOn;
+            if (genres) data['genres'] = genres;
+            const result = await AuthorDBNew.findByIdAndUpdate({ _id: authorId }, { [`books.${title}`]: data  }, { new: true });
+            // const result = {}
+            status = 200
+            response = createResponse(result, 'Added Book to Author')
         } catch (error) {
             status = 500
             response = handelError(error)
@@ -157,9 +173,7 @@ class AuthorUpdatedController implements AuthorController {
         let response: ApiResponse<any>;
         let status: number;
         try {
-            ``
-            const authorId: string = req.params.authorId
-            await validateAuthorId(authorId)
+            const authorId = await fetchAuthorId(req)
             const title: string = req.params.bookTitle
             const result: DeleteResult = await AuthorDBNew.updateOne({ _id: authorId }, { $unset: { [`books.${title}`]: 1 } });
             status = 200
@@ -175,3 +189,14 @@ class AuthorUpdatedController implements AuthorController {
 const authorController = new AuthorUpdatedController()
 export default authorController
 
+/*
+
+1 reduce into function
+    const authorId: string = req.params.authorId
+    await isValidAuthorId(authorId)
+2 author name duplication validation
+3 return updated fields
+
+4 PUT for adding book
+
+*/
